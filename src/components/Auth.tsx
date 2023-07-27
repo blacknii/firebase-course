8a2a3aeae0e217881d535963d771cbd789fd42fd
blacknii <42908977+blacknii@users.noreply.github.com>
2023-07-27 12:55:59 +0200

    Setting up Authentication

---
+++ src/components/Auth.tsx
@@ -0,0 +1,63 @@
+import { auth, googleProvider } from "../config/firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { ChangeEvent, useState } from "react";
+
+export const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  console.log(auth.currentUser?.photoURL);
+
+  const singIn = async () => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const singInWithGoogle = async () => {
+    try {
+      await signInWithPopup(auth, googleProvider);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      await signOut(auth);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleEmail = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePassword = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  return (
+    <>
+      <input value={email} onChange={handleEmail} placeholder="Email..." />
+      <input
+        value={password}
+        onChange={handlePassword}
+        type="password"
+        placeholder="Password..."
+      />
+      <button onClick={singIn}>Sign In</button>
+
+      <button onClick={singInWithGoogle}>Sign In With Google</button>
+
+      <button onClick={logout}>Logout</button>
+    </>
+  );
+};
---
+++ src/config/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDILwakZziWbFAnx6lq9gzIhFwYYd_O40U",
+  authDomain: "fir-course-3bc59.firebaseapp.com",
+  projectId: "fir-course-3bc59",
+  storageBucket: "fir-course-3bc59.appspot.com",
+  messagingSenderId: "516010320593",
+  appId: "1:516010320593:web:8075e88b76469b0bfadbf0",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
